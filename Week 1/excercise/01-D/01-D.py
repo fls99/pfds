3bd0b2726bade0da532b82c3f9e49bf069110fca
Felix Steppack <felix.steppack@googlemail.com>
2024-11-04 19:07:44 +0100

    change input

--- Week 1/excercise/01-D/01-D.py
+++ Week 1/excercise/01-D/01-D.py
@@ -1,10 +1,11 @@
 def betrayers_badge(list_num: list[int], badge_num: int) -> list[int]:
-    list_num.pop()
-    list_num.pop(badge_num)
+    if 0 <= badge_num < len(list_num):
+        list_num.pop(badge_num)
     return [i for i in list_num if i != badge_num]
 
 
 
-x: list[int] = list(map(int, input().split()))
-output = betrayers_badge(x, x[-1])
+ancient_scroll = list(map(int, input().split()))
+badge = int(input())
+output = betrayers_badge(ancient_scroll, badge)
 print(output)
