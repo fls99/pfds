83a14d7b499ed2362aa186e4bd8c8c3dfb287884
Felix Steppack <felix.steppack@googlemail.com>
2024-11-04 18:09:17 +0100

    return max power of the clan who has won more battles

--- Week 1/excercise/01-C/01-C.py
+++ Week 1/excercise/01-C/01-C.py
@@ -11,10 +11,10 @@ def split_list(a_list:list[int]):
     return a_list[:half], a_list[half:]
 
 
-def rune_rumble(runes:list) -> int:
+def rune_rumble(runes:list[int]) -> int:
     """
     Compares the rune in a 1:1 battle for each clan and returns the total power of all matches won
-    
+
     @runes: A list containing the power of each clan
     @return: Returns the winner of the battle indicated as the maximum power of all won battles
     """
@@ -22,14 +22,22 @@ def rune_rumble(runes:list) -> int:
 
     counter_griffon: int = 0
     counter_phoenix: int = 0
+    battles_griffon: int = 0
+    battles_phoenix: int = 0
 
     for i in range(len(runes_griffon)):
         if runes_griffon[i] > runes_phoenix[i]:
             counter_griffon += runes_griffon[i]
+            battles_griffon += 1
         else:
             counter_phoenix += runes_phoenix[i]
+            battles_phoenix += 1
     
-    return max(counter_phoenix, counter_griffon)
+    if battles_griffon > battles_phoenix:
+        return counter_griffon
+    else:
+        return counter_phoenix
+
 
 runes = list(map(int, input().split()))
 x = rune_rumble(runes)
